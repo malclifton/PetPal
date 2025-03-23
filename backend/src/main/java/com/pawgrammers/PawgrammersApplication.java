7b245dfa4fa8fc7d53836839c2ef5942e9fa274f
kp0430 <122626179+kp0430@users.noreply.github.com>
2025-03-23 15:33:30 -0400

    S3 implementation

---
+++ backend/src/main/java/com/pawgrammers/PawgrammersApplication.java
@@ -0,0 +1,13 @@
+package com.pawgrammers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawgrammersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawgrammersApplication.class, args);
+	}
+
+}
